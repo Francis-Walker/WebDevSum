6de290c7096f7ba30b2a1aa703da9491dfd61080
Francis-Walker <55081670+Francis-Walker@users.noreply.github.com>
2020-05-14 15:17:40 +0200

    Update db_connect.php

--- includes/db_connect.php
+++ includes/db_connect.php
@@ -1,10 +1,12 @@
 <?php
+ini_set('display_errors', 1);
 include('constants.php');
 function connectToDb()
 {
 
     $mysqli = mysqli_connect(HOST_NAME, HOST_USERNAME, HOST_PASSWORD, DB_NAME);
 
+    
 
     if (mysqli_connect_errno()) {
         echo "Unable to connect to database" . mysqli_error($mysqli);
