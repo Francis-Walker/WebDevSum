32740ec98b6f83cbc4020861ed36c0de28f9da0f
Francis-Walker <55081670+Francis-Walker@users.noreply.github.com>
2020-05-14 15:19:41 +0200

    Update constants.php

--- includes/constants.php
+++ includes/constants.php
@@ -1,6 +1,6 @@
 <?php
 define('HOST_NAME', 'eu-cdbr-west-03.cleardb.net');
-define('HOST_USERNAME', 'b7adcbf35657e2:2c7ac8e4');
+define('HOST_USERNAME', 'b7adcbf35657e2');
 define('DB_NAME', 'heroku_0854f16e2547851');
 define('HOST_PASSWORD', '2c7ac8e4');
 //rt)@Se4_prF#Z6(f
