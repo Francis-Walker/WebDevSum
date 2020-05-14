d61b8c7cb17362f719be2acdd7eefb742094a046
Francis-Walker <55081670+Francis-Walker@users.noreply.github.com>
2020-05-14 14:45:34 +0200

    Update constants.php

--- includes/constants.php
+++ includes/constants.php
@@ -1,7 +1,8 @@
 <?php
-define('HOST_NAME', 'localhost');
-define('HOST_USERNAME', 'root');
-define('DB_NAME', 'webdevproject');
-define('HOST_PASSWORD', '');
+define('HOST_NAME', 'eu-cdbr-west-03.cleardb.net');
+define('HOST_USERNAME', 'b7adcbf35657e2:2c7ac8e4');
+define('DB_NAME', 'heroku_0854f16e2547851');
+define('HOST_PASSWORD', '2c7ac8e4');
 //rt)@Se4_prF#Z6(f
+//CLEARDB_DATABASE_URL: mysql://b7adcbf35657e2:2c7ac8e4@eu-cdbr-west-03.cleardb.net/heroku_0854f16e2547851?reconnect=true
 ?>
